7275bf1562f569d864f142fb4dca47b6bd08b3fc
RamonER <esparza.ramon@proton.me>
2025-02-24 20:01:52 -0600

    removed artifacts endpoint thinking for something else

--- server/server.go
+++ server/server.go
@@ -10,8 +10,6 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
 var globalConfig config.Config
@@ -41,58 +39,6 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	tmpl.Execute(w, scripts)
-}
-
-func uploadImageHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-	// Limit file size to 10MB. This line saves you from those accidental 100MB uploads!
-	r.ParseMultipartForm(10 << 20)
-
-	// Retrieve the file from form data
-	file, handler, err := r.FormFile("artifact")
-	if err != nil {
-		http.Error(w, "Error retrieving the file", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-
-	fmt.Fprintf(w, "Uploaded File: %s\n", handler.Filename)
-	fmt.Fprintf(w, "File Size: %d\n", handler.Size)
-	fmt.Fprintf(w, "MIME Header: %v\n", handler.Header)
-
-	// Now let’s save it locally
-	dst, err := ops.CreateFile(handler.Filename)
-	if err != nil {
-		http.Error(w, "Error saving the file", http.StatusInternalServerError)
-		return
-	}
-	defer dst.Close()
-
-	// Copy the uploaded file to the destination file
-	if _, err := dst.ReadFrom(file); err != nil {
-		http.Error(w, "Error saving the file", http.StatusInternalServerError)
-	}
-}
-
-// Handler to serve images
-func serveImageHandler(w http.ResponseWriter, r *http.Request) {
-	filename := r.URL.Path[len("./uploads/"):] // Extract filename
-	imageDir := "./uploads"
-	imagePath := filepath.Join(imageDir, filename)
-	fmt.Println(imagePath)
-	// Check if file exists
-	if _, err := os.Stat(imagePath); os.IsNotExist(err) {
-		fmt.Println(err)
-		http.Error(w, "Image not found", http.StatusNotFound)
-		return
-	}
-
-	// Serve the image
-
-	http.ServeFile(w, r, imagePath)
 }
 
 func executeHandler(w http.ResponseWriter, r *http.Request) {
@@ -162,8 +108,6 @@ func MainServer(cfg config.Config) {
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/execute", executeHandler)
-	http.HandleFunc("/upload", uploadImageHandler)
-	http.HandleFunc("/artifacts/", serveImageHandler)
 
 	http.ListenAndServe(port, nil)
 }
